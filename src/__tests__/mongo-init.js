5ef0f85dbc83979bb540827aaf64a89d6e5bbf23
mateszadam <matesz.adam@gmail.com>
2025-02-20 09:14:06 +0100

    fix: add MongoDB initialization script and mount it in CI workflow

---
+++ src/__tests__/mongo-init.js
@@ -0,0 +1,9 @@
+db.user.insertMany([
+	{
+		_id: ObjectId('5f6793bb6219bff92baf980ade'),
+		name: 'admin',
+		password: '$2b$12$M22TIo7SLYycMU6ODOjlnuD6vXktrG8do9DX/TTELQgCMvd1pp7Dq',
+		role: 'admin',
+		profilePicture: 'cool-svgrepo-com.svg',
+	},
+]);
