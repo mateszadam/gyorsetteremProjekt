99d0749181bcf893439ec44bb360e9a62cc773e6
mateszadam <matesz.adam@gmail.com>
2025-02-25 14:57:58 +0100

    feat: add user profile picture change and admin registration tests, update error messages

--- src/__tests__/userController.test.js
+++ src/__tests__/userController.test.js
@@ -71,4 +71,110 @@ describe('userController Integration Tests', () => {
 			expect(response.status).toBe(200);
 		});
 	});
+	describe('05 PUT /user/picture/change/:newImageName', () => {
+		it('should change the profile picture of a user', async () => {
+			const newImageName = 'angel-svgrepo-com.svg';
+			const response = await request(baseUrl)
+				.put(`/user/picture/change/${newImageName}`)
+				.set('Authorization', `Bearer ${token}`);
+			expect(response.status).toBe(200);
+		});
+
+		it('should change the profile picture when image not exist', async () => {
+			const newImageName = 'newProfilePic.jpg';
+			const response = await request(baseUrl)
+				.put(`/user/picture/change/${newImageName}`)
+				.set('Authorization', `Bearer ${token}`);
+			expect(response.status).toBe(400);
+			expect(response.body).toEqual({ message: 'Image does not exist!' });
+		});
+
+		it('should return error if new image name does not exist', async () => {
+			const newImageName = 'nonExistentPic.jpg';
+			const response = await request(baseUrl)
+				.put(`/user/picture/change/${newImageName}`)
+				.set('Authorization', `Bearer ${token}`);
+			expect(response.status).toBe(400);
+		});
+	});
+	describe('06 POST /user/register/admin', () => {
+		it('should register a new admin', async () => {
+			const response = await request(baseUrl)
+				.post('/user/register/admin')
+				.set('Authorization', `Bearer ${token}`)
+				.send({
+					name: 'TestAdmin',
+					password: 'Admin@1234',
+					email: 'testadmin@example.com',
+					role: 'admin',
+				});
+			expect(response.status).toBe(201);
+		});
+		it('should register a new kitchen', async () => {
+			const response = await request(baseUrl)
+				.post('/user/register/admin')
+				.set('Authorization', `Bearer ${token}`)
+				.send({
+					name: 'TestAdmin2',
+					password: 'Admin@1234',
+					email: 'testadmin@example.com',
+					role: 'kitchen',
+				});
+			expect(response.status).toBe(201);
+		});
+		it('should register a new salesman', async () => {
+			const response = await request(baseUrl)
+				.post('/user/register/admin')
+				.set('Authorization', `Bearer ${token}`)
+				.send({
+					name: 'TestAdmin3',
+					password: 'Admin@1234',
+					email: 'testadmin@example.com',
+					role: 'salesman',
+				});
+			expect(response.status).toBe(201);
+		});
+
+		it('should return error if role is invalid', async () => {
+			const response = await request(baseUrl)
+				.post('/user/register/admin')
+				.set('Authorization', `Bearer ${token}`)
+				.send({
+					name: 'InvalidRoleUser',
+					password: 'Invalid@1234',
+					email: 'invalidrole@example.com',
+					role: 'invalidRole',
+				});
+			expect(response.status).toBe(400);
+			expect(response.body).toEqual({
+				message: 'The provided permission does not exist!',
+			});
+		});
+
+		it('should return error if password is not provided', async () => {
+			const response = await request(baseUrl)
+				.post('/user/register/admin')
+				.set('Authorization', `Bearer ${token}`)
+				.send({
+					name: 'NoPasswordUser',
+					email: 'nopassword@example.com',
+					role: 'admin',
+				});
+			expect(response.status).toBe(400);
+			expect(response.body).toEqual({ message: 'Password is required!' });
+		});
+
+		it('should return error if email is not provided', async () => {
+			const response = await request(baseUrl)
+				.post('/user/register/admin')
+				.set('Authorization', `Bearer ${token}`)
+				.send({
+					name: 'NoEmailUser',
+					password: 'NoEmail@1234',
+					role: 'admin',
+				});
+			expect(response.status).toBe(400);
+			expect(response.body).toEqual({ message: 'Email is required!' });
+		});
+	});
 });
