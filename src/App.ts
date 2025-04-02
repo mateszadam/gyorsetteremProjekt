a288fb0890320ee56e813d694bfa709f5cdc46f0
mateszadam <matesz.adam@gmail.com>
2025-04-02 09:51:58 +0200

    refactor: enhance MongoDB connection handling with retry logic and improved error messages

--- src/App.ts
+++ src/App.ts
@@ -73,14 +73,35 @@ class App {
 
 	private connectToTheDatabase(mongoUri: string) {
 		mongoose.set('strictQuery', true);
-		mongoose
-			.connect(mongoUri)
-			.catch(() =>
-				console.log(
+		mongoose.connect(mongoUri).catch(() =>
+			console
+				.log(
 					'\x1b[41m%s\x1b[0m',
 					'Unable to connect to the server. Please start MongoDB.'
 				)
-			);
+				.then(() => {
+					// Connection successful, nothing to do here
+				})
+				.catch((err) => {
+					console.log(
+						'\x1b[41m%s\x1b[0m',
+						'Unable to connect to the server. Please start MongoDB.: ' +
+							err.message
+					);
+					console.log('Retrying in 10 seconds...');
+
+					setTimeout(() => {
+						console.log('Attempting to reconnect to MongoDB...');
+						mongoose.connect(mongoUri).catch((retryErr) => {
+							console.log(
+								'\x1b[41m%s\x1b[0m',
+								'Reconnection failed:',
+								retryErr.message
+							);
+						});
+					}, 10000);
+				})
+		);
 
 		mongoose.connection.on('error', (error) => {
 			console.log(
